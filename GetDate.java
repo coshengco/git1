0e78f8a066c834ceeef64a398dc7544cba840b3e
coshengco <2393925149@qq.com>
2016-05-13 23:08:43 +0800

    'one'

---
+++ GetDate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class GetDate { // 新建类
+	public static void main(String[] args) { // 主方法
+		Date date = new Date(); // 创建Date对象date
+		String year = String.format("%tY", date); // 将date进行格式化
+		String month = String.format("%tB", date);
+		String day = String.format("%td", date);
+		//System.out.println("今年是：" + year + "年"); // 输出信息
+		//System.out.println("现在是：" + month);
+		System.out.println("今天是：" + day + "号");String hour = String.format("%tH", date); // 将date进行格式化
+		String minute = String.format("%tM", date);
+		String second = String.format("%tS", date);
+		 // 输出的信息
+		System.out.println("现在是：" + hour + "时" + minute + "分"
+				+ second + "秒");
+	}
+}
+
+		
